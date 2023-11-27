9f99504e6fd9fb5d56b486192646d90f1ee75186
Hank Xu <uuuuuuxuninghua@gmail.com>
2023-11-27 02:11:52 -0500

    library initial design + floating books

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.VFX;
 
 /// <summary>
 /// The main partial class for Player
@@ -15,13 +16,12 @@ public partial class Player : MonoBehaviour
 {
     [Header("Fields")]
     [SerializeField] private CinemachineVirtualCamera virtualCamera;
+
     public static Player Instance { get; private set; }
 
     private void Awake()
     {
         Instance = this;
-        
-        controller = GetComponent<CharacterController>();
     }
 
     private void Start()
@@ -31,14 +31,10 @@ public partial class Player : MonoBehaviour
 
     private void Update()
     {
-        HandleMovement();
     }
 
-    #region TRIGGER FUNCTIONS
-    
     private void OnInteract()
     {
         Interact();
     }
-    #endregion
 }
